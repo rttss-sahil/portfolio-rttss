d77f4cbdd8e12d69f506ab58d95160f6cdd66017
rttss <sahil.git@outlook.com>
2020-10-15 02:50:31 +0530

    building

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,27 +1,27 @@
 const CACHE_NAME = "v1.1.3",
   urlsToCache = [
-    "./index.html",
-    "./assets/img/bg-main.jpg",
-    "./assets/img/color-picker.png",
-    "./assets/img/cursor.png",
-    "./assets/img/figure-1.png",
-    "./assets/img/figure-2.png",
-    "./assets/img/figure-3.png",
-    "./assets/img/hand.png",
-    "./assets/img/heroku.png",
-    "./assets/img/main-pic-1.png",
-    "./assets/img/main-pic-2.png",
-    "./assets/img/main-pic-3.png",
-    "./assets/img/main-pic-4.png",
-    "./assets/img/main-pic-5.png",
-    "./assets/img/main-pic-6.png",
-    "./assets/img/main-pic-7.png",
-    "./assets/img/mongodb.png",
-    "./assets/img/node.png",
-    "./assets/img/project-1.jpg",
-    "./assets/img/react.png",
-    "./assets/img/redux.png",
-    "./assets/img/sass.png",
+    "index.html",
+    "assets/img/bg-main.jpg",
+    "assets/img/color-picker.png",
+    "assets/img/cursor.png",
+    "assets/img/figure-1.png",
+    "assets/img/figure-2.png",
+    "assets/img/figure-3.png",
+    "assets/img/hand.png",
+    "assets/img/heroku.png",
+    "assets/img/main-pic-1.png",
+    "assets/img/main-pic-2.png",
+    "assets/img/main-pic-3.png",
+    "assets/img/main-pic-4.png",
+    "assets/img/main-pic-5.png",
+    "assets/img/main-pic-6.png",
+    "assets/img/main-pic-7.png",
+    "assets/img/mongodb.png",
+    "assets/img/node.png",
+    "assets/img/project-1.jpg",
+    "assets/img/react.png",
+    "assets/img/redux.png",
+    "assets/img/sass.png",
   ],
   self = this;
 
@@ -41,8 +41,18 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches
       .match(e.request)
-      .then((res) => res || fetch(e.request))
-      .catch(() => caches.match("/index.html"))
+      .then((res) => {
+        if (res) {
+          return res;
+        } else {
+          return fetch(e.request);
+        }
+      })
+
+      .catch(() => {
+        console.log("matching with index");
+        return caches.match("index.html");
+      })
   );
 });
 
@@ -57,7 +67,7 @@ self.addEventListener("activate", (e) => {
         Promise.all(
           cacheNames.map(
             (cacheName) =>
-              cacheWhitelist.includes(cacheName) && caches.delete(cacheName)
+              !cacheWhitelist.includes(cacheName) && caches.delete(cacheName)
           )
         )
       )
